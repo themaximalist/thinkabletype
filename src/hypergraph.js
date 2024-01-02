c6666594875bdcd7fa439549dc5dd0805fbe9ee5
Brad Jasper <bjasper@gmail.com>
2024-01-01 16:12:40 -0800

    refactoring into node and hyperedge objects

--- src/hypergraph.js
+++ src/hypergraph.js
@@ -1,4 +1,72 @@
+const uuidv4 = require("uuid/v4");
+
+class Node {
+    constructor(node) {
+        this.node = node;
+        this.uuid = uuidv4();
+    }
+}
+
 export default class Hypergraph {
+    constructor(hyperedges) {
+        this.reset();
+        this.load(hyperedges);
+    }
+
+    get hyperedges() {
+        return Object.values(this._hyperedges);
+    }
+
+    reset() {
+        this._hyperedges = {};
+        this.nodes = new Set();
+    }
+
+    load(hyperedges = []) {
+        this.reset();
+
+        for (const hyperedge of hyperedges) {
+            this.addHyperedge(hyperedge);
+        }
+    }
+
+    get(node_or_hyperedge) {
+        if (Array.isArray(node_or_hyperedge)) {
+            return this._hyperedges[node_or_hyperedge];
+        } else {
+            return this.nodes[node_or_hyperedge];
+        }
+    }
+
+    has(node_or_hyperedge) {
+        if (Array.isArray(node_or_hyperedge)) {
+            return this._hyperedges[node_or_hyperedge] !== undefined;
+        } else {
+            return this.nodes.has(node_or_hyperedge);
+        }
+    }
+
+    add(node_or_hyperedge) {
+        if (Array.isArray(node_or_hyperedge)) {
+            this.addHyperedge(node_or_hyperedge);
+        } else {
+            this.addNode(node_or_hyperedge);
+        }
+    }
+
+    addNode(node) {
+        this.nodes.add(node);
+    }
+
+    addHyperedge(hyperedge) {
+        for (const node of hyperedge) {
+            this.addNode(node);
+        }
+
+        this._hyperedges[hyperedge] = hyperedge;
+    }
+
+    /*
     constructor(hyperedges) {
         this.hyperedges = new Set();
         this.nodes = new Set();
@@ -6,16 +74,6 @@ export default class Hypergraph {
         this.load(hyperedges);
     }
 
-    load(hyperedges = []) {
-        this.hyperedges = new Set(hyperedges);
-        this.nodes = new Set();
-
-        for (const hyperedge of hyperedges) {
-            for (const node of hyperedge) {
-                this.nodes.add(node);
-            }
-        }
-    }
 
     add(node_or_hyperedge) {
         if (Array.isArray(node_or_hyperedge)) {
@@ -28,4 +86,5 @@ export default class Hypergraph {
             this.nodes.add(node_or_hyperedge);
         }
     }
+    */
 }
--- test/hypergraph_test.js
+++ test/hypergraph_test.js
@@ -1,8 +1,8 @@
-// set.has doesn't work for arrays
-// set doesn't de-duplicate arrays
 // give it a uuid
 // select the node as an object and perform operations on it (closest neighboors)
 // page rank
+// embedding
+// implicit hyperedges (computational AI graph)
 
 const assert = require("assert");
 
@@ -15,22 +15,73 @@ describe("Hypergraph", function () {
     assert(graph.nodes.size == 0);
   });
 
+  /*
   it("add node", function () {
     const graph = new Hypergraph();
-    graph.add(1);
+    graph.add("A");
+    graph.add("A"); // dupe
     assert(graph.nodes.size == 1);
-    assert(graph.nodes.has(1));
+    assert(graph.nodes.has("A"));
+
+    graph.add("B");
+    graph.add("B"); // dupe
+    assert(graph.nodes.size == 2);
+    assert(graph.nodes.has("B"));
   });
 
   it("add hyperedge", function () {
     const graph = new Hypergraph();
-    graph.add([1, 2, 3]);
-    assert(graph.nodes.size == 3);
-    assert(graph.nodes.has(1));
-    assert(graph.nodes.has(2));
-    assert(graph.nodes.has(3));
-    assert(graph.hyperedges.size == 1);
-    console.log(graph.hyperedges);
-    assert(graph.hyperedges.has([1, 2, 3]));
+    graph.add(["A", "B"]);
+    graph.add(["A", "B"]);
+    assert(graph.nodes.size == 2);
+    assert(graph.nodes.has("A"));
+    assert(graph.nodes.has("B"));
+
+    assert(graph.hyperedges.length == 1);
   });
+
+  it("add multiple hyperedge", function () {
+    const graph = new Hypergraph();
+    graph.add(["A", "B"]);
+    graph.add(["A", "B", "C"]);
+    assert(graph.nodes.size == 3);
+    assert(graph.nodes.has("A"));
+    assert(graph.nodes.has("B"));
+    assert(graph.nodes.has("C"));
+
+    assert(graph.hyperedges.length == 2);
+  });
+
+  it("has hyperedge", function () {
+    const graph = new Hypergraph();
+    graph.add(["A", "B"]);
+
+    assert(graph.has("A"));
+    assert(graph.has("B"));
+    assert(graph.has(["A", "B"]));
+    assert(!graph.has(["A", "C"]));
+  });
+
+  it("load", function () {
+    const graph = new Hypergraph([
+      ["A", "B"],
+      ["A", "B", "C"],
+    ]);
+
+    assert(graph.has("A"));
+    assert(graph.has("B"));
+    assert(graph.has(["A", "B"]));
+    assert(!graph.has(["A", "C"]));
+  });
+
+  it("get node", function () {
+    const graph = new Hypergraph();
+    graph.add(["A", "B"]);
+    const nodeA = graph.get("A");
+    console.log("NODE", nodeA);
+  });
+  */
+
+  // get hyperedge
+  // uuid
 });
