2ef2eca0571871b04b1e933bfbc9f496c93b1b2c
Brad Jasper <bjasper@gmail.com>
2024-01-06 00:16:46 -0800

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Hypergraph from "./hypergraph.js";
 
+export default Hypergraph;
+
+/*
 async function main() {
     const graph = Hypergraph.parse(`Ted Nelson invented hypertext
 Ted Nelson invented hypermedia
@@ -16,12 +19,4 @@ Ted Nelson invented Xanadu`);
 }
 
 main();
-
-// TODO: node.similar() should return similar to that node
-// TODO: hyperedge.similar() should return similar to that hyperedge (...how to do this?)
-//        - do we need to do a hyperedge embedding? --> yes
-//        - do any of these nodes form a known hyper edge? if so, what are the longest ones?
-// TODO: needs more tests...super buggy
-
-// TODO: therefore we need embeddings to be better. faster. ideally not async...but need another way to "load" a node and not have it leak everywhere.
-//        - just be explicit. anywhere a node is created, it needs to be loaded. same with a hyperedge. cache. cache. cache. test. test. test.
+*/
