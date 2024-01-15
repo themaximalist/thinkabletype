03d1994bbacf575bd6699a3a4d8a7106acc0abad
Brad Jasper <bjasper@gmail.com>
2024-01-14 16:37:07 -0800

    simple

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-import Hypergraph from "./hypergraph.js";
-export default Hypergraph;
+import VectorDB from "@themaximalist/vectordb-lite.js";
+
+export default class Hypergraph {
+    constructor() {
+        this.vectordb = new VectorDB();
+        console.log("INIT HYPERGRAPH");
+    }
+}
+
+//import Hypergraph from "./hypergraph.js";
+//export default Hypergraph;
